255691bfa9e28926dbdc2eccba545f23db388641
vaibhav <codekul.vaibhav@gmail.com>
2021-10-22 16:05:37 +0530

    Star patterns

--- src/controlflowstatement/StarPattern.java
+++ src/controlflowstatement/StarPattern.java
@@ -1,31 +1,130 @@
 package controlflowstatement;
 
 
-
-/**
- *  * * * *
- *  * * * *
- *  * * * *
- *  * * * *
-
- */
 public class StarPattern {
 
     public static void main(String[] args) {
-        for(int i=1;i<=4;i++){
-            for(int j=1;j<=5;j++){
+
+        /**
+         *  * * * *
+         *  * * * *
+         *  * * * *
+         *  * * * *
+
+         */
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 5; j++) {
                 System.out.print(" *");
             }
             System.out.println();
         }
-    }
-}
+
 
 /**
  *  *
  *  * *
  *  * * *
  *  * * * *
- *
- *
+
  */
+
+        System.out.println();
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+/**
+ *  * * * *
+ *  * * *
+ *  * *
+ *  *
+
+ */
+
+        System.out.println();
+        for (int i = 1; i <= 4; i++) {
+            for (int j = i; j <= 4; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+
+
+        /**
+         *         *
+         *       * *
+         *     * * *
+         *   * * * *
+         */
+        System.out.println();
+        for (int i = 1; i <= 4; i++) {
+            for (int j = i; j <= 4; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        /**
+         *        *
+         *       * *
+         *      * * *
+         *     * * * *
+         *
+         */
+
+        System.out.println();
+        for (int i = 1; i <= 4; i++) {
+            for (int j = i; j <= 4; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+
+        /**
+         *   * * * *
+         *     * * *
+         *       * *
+         *         *
+         *
+         */
+        System.out.println();
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for(int k=i;k<=4;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        /**
+         *    * * * *
+         *     * * *
+         *      * *
+         *       *
+         */
+        System.out.println();
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for(int k=i;k<=4;k++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+
+
